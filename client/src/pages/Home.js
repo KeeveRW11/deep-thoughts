d027d87962786c61c9b2baad9581e555633efa3c
KeeveRW11 <keevewhyte+github@gmail.com>
2022-11-20 22:38:15 -0500

    add form for thoughts

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,8 +1,11 @@
 import React from 'react';
+
+import ThoughtForm from '../components/ThoughtForm';
 import ThoughtList from '../components/ThoughtList';
 import FriendList from '../components/FriendList';
 
 import { useQuery } from '@apollo/client';
+
 
 
 
@@ -24,6 +27,11 @@ const Home = () => {
   return (
     <main>
       <div className='flex-row justify-space-between'>
+        {loggedIn && (
+          <div className='col-12 mb-3'>
+            <ThoughtForm />
+          </div>  
+        )}
         <div className={`col-12 mb-3 ${loggedIn && 'col-lg-8'}`}>
           {loading ? (
             <div>Loading...</div>
